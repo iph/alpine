da8e84c87b3b273021a30a9324fcc0b6e1f5fc93
Sean Tyler Myers <seamyers@amazon.com>
2019-05-18 18:33:07 -0700

    Remove ecs linked role

--- terraform/fargate.tf
+++ terraform/fargate.tf
@@ -29,30 +29,25 @@ resource "aws_ecs_task_definition" "app" {
 DEFINITION
 }
 
-resource "aws_iam_service_linked_role" "ecs" {
-  aws_service_name = "ecs.amazonaws.com"
-}
-
 resource "aws_ecs_service" "main" {
-  name            = "${var.tag}-service"
-  cluster         = "${aws_ecs_cluster.main.id}"
+  name = "${var.tag}-service"
+  cluster = "${aws_ecs_cluster.main.id}"
   task_definition = "${aws_ecs_task_definition.app.arn}"
-  desired_count   = "${var.app_count}"
-  launch_type     = "FARGATE"
+  desired_count = "${var.app_count}"
+  launch_type = "FARGATE"
 
   network_configuration {
     security_groups = ["${module.vpc.sg-alb-to-fe}"]
-    subnets         = "${module.vpc.private-subnets}"
+    subnets = "${module.vpc.private-subnets}"
   }
 
   load_balancer {
     target_group_arn = "${aws_alb_target_group.app.id}"
-    container_name   = "app"
-    container_port   = "${var.app_port}"
+    container_name = "app"
+    container_port = "${var.app_port}"
   }
 
   depends_on = [
     "aws_alb_listener.front_end",
-    "aws_iam_service_linked_role.ecs"
   ]
 }
