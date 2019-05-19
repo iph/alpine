5c251a78bd92ec617239f9e1ad6aaecb24475fd6
Sean Tyler Myers <seamyers@amazon.com>
2019-05-18 17:48:31 -0700

    Add redirects to alb for HTTP

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -27,3 +27,30 @@ resource "aws_alb_listener" "front_end" {
   }
 }
 
+resource "aws_alb_listener" "redirect" {
+  load_balancer_arn = "${aws_alb.main.id}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_route53_record" "sub-alb-record" {
+  zone_id = "${module.routes.sub-record}"
+  name    = "${module.routes.sub-domain}"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_alb.main.dns_name}"
+    zone_id                = "${aws_alb.main.zone_id}"
+    evaluate_target_health = false
+  }
+}
